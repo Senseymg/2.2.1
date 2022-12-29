d85f94af81c34a95adc21522fd7928f303b1414d
LordKiraell <senseymg@gmail.com>
2022-12-30 01:57:14 +0300

    Initial

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImpl.java
@@ -0,0 +1,27 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+@Repository
+public class CarDaoImpl implements CarDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void addCar (Car car) {
+        sessionFactory.getCurrentSession().save(car);
+
+    }
+
+   /* @Override
+    public User getCarOwner(String model, int series) {
+        TypedQuery<User> query=sessionFactory.getCurrentSession().;
+        return query.getResultList();
+    }*/
+}
